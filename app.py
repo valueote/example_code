0b3d6b14b89aaae43f6f1aa7a32691e9119d0531
vivy <2686220852@qq.com>
2024-07-19 18:44:48 +0800

    change front

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
-import webbrowser
 from flask import Flask, request, jsonify, send_from_directory
 from flask_cors import CORS
 import os
-# Set environment variables
 os.environ["SERPAPI_API_KEY"] = "7c568f6675a4d131a3e98359a6a58a82ed7d752c908f3e4ad9a68f6e443deb15"
 os.environ["USER_AGENT"] = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36 Edg/126.0.0.0"
 
+import webbrowser
 from langchain_community.chat_models import ChatSparkLLM
 from langchain_core.prompts import ChatPromptTemplate
 from langchain.chains.combine_documents import create_stuff_documents_chain
@@ -39,6 +38,7 @@ from langchain.schema import (
 app = Flask(__name__, static_folder='static')
 CORS(app)
 
+# Set environment variables
 
 # Chat model config
 chat_model = ChatSparkLLM(
@@ -54,21 +54,18 @@ chat_model = ChatSparkLLM(
 
 # load website
 loader = WebBaseLoader(
-    web_path=
-    "https://web.stanford.edu/class/cs106l/index.html"
+    web_path="https://web.stanford.edu/class/cs106l/index.html"
 )
 # load website into Document
 docs = loader.load()
 
 EMBEDDING_DEVICE = "cpu"
-embeddings = HuggingFaceEmbeddings(model_name="/home/vivy/ai/m3e-base",
-                                   model_kwargs={'device': EMBEDDING_DEVICE})
+embeddings = HuggingFaceEmbeddings(model_name="/home/vivy/ai/m3e-base", model_kwargs={'device': EMBEDDING_DEVICE})
 
 text_splitter = RecursiveCharacterTextSplitter()
 documents = text_splitter.split_documents(documents=docs)
 vector = FAISS.from_documents(documents=documents, embedding=embeddings)
 retriever = vector.as_retriever()
-
 
 # prompt for chat model
 prompt = ChatPromptTemplate.from_messages([
@@ -79,7 +76,6 @@ prompt = ChatPromptTemplate.from_messages([
 
 retriever_chain = create_history_aware_retriever(chat_model, retriever, prompt)
 
-
 prompt = ChatPromptTemplate.from_messages([
     ("system", "Answer the user's questions based on the below context:\n\n{context}"),
     MessagesPlaceholder(variable_name="chat_history"),
@@ -89,40 +85,35 @@ prompt = ChatPromptTemplate.from_messages([
 document_chain = create_stuff_documents_chain(chat_model, prompt)
 retrieval_chain = create_retrieval_chain(retriever_chain, document_chain)
 
-
 chat_history = [
     HumanMessage(content="do you know my name"),
     AIMessage(content="sorry, i don't know your name")
 ]
-chat_history.append(HumanMessage(content="please remember my name, i'm anvery"))
-chat_history.append(AIMessage(content="ok anvery, i will remember your name"))
+chat_history.append(HumanMessage(content="please remember my name, i'm vivy"))
+chat_history.append(AIMessage(content="ok vivy, i will remember your name"))
 
-
-
-#create Agent
+# Create Agent
 from langchain.agents import Tool
 from langchain.chains import LLMMathChain
-from langchain.agents import Tool
 
-llm_math = LLMMathChain(llm = chat_model)
+llm_math = LLMMathChain(llm=chat_model)
 
-# initialize the math tool
+# Initialize the math tool
 math_tool = Tool(
-    name ='Calculator',
-    func = llm_math.run,
-    description ='Useful for when you need to answer questions about math.'
+    name='Calculator',
+    func=llm_math.run,
+    description='Useful for when you need to answer questions about math.'
 )
-# when giving tools to LLM, we must pass as list of tools
+# When giving tools to LLM, we must pass as list of tools
 tools = [math_tool]
-
 
 from langchain import hub
 
 prompt = hub.pull("hwchase17/react")
-model = chat_model
-
 agent = create_react_agent(llm=chat_model, tools=tools, prompt=prompt)
 agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=False)
+
+
 
 @app.route('/')
 def index():
@@ -142,9 +133,14 @@ def ask():
         })
         ai_message = response["answer"]
 
+    # 处理换行符
+    ai_message = ai_message.replace('\n', '\n\n')
+
+    chat_history.append(HumanMessage(content=user_message))
     chat_history.append(AIMessage(content=ai_message))
-    return jsonify({"answer": ai_message})
+
+    return jsonify({'answer': ai_message})
 
 if __name__ == '__main__':
-    webbrowser.open("http://127.0.0.1:5000")
+    webbrowser.open_new('http://127.0.0.1:5000')
     app.run(debug=True)
